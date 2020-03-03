046599b2470e8cecbab73ee51d896a96d36d3d06
pankajsoni166 <pankajsoni166@gmail.com>
2020-03-03 15:17:58 +0530

    Changes to support kubernetes service discovery

--- src/main/java/com/pramati/userservice/UserServiceApplication.java
+++ src/main/java/com/pramati/userservice/UserServiceApplication.java
@@ -2,21 +2,18 @@ package com.pramati.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
--- src/main/java/com/pramati/userservice/controller/UserController.java
+++ src/main/java/com/pramati/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pramati.userservice.controller;
 
 import com.pramati.userservice.model.MyUser;
+import com.pramati.userservice.repository.UserRepository;
 import com.pramati.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,9 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/all")
     public List<MyUser> getAllUsers(){
         return userService.getAllUser();
@@ -27,6 +32,21 @@ public class UserController {
 
     @RequestMapping("/bookdoctor/{doctorId}")
     public String bookDoctor(@PathVariable("doctorId") int doctorId){
-        return restTemplate.getForObject("http://booking-service/doctor/book/"+doctorId,String.class);
+        return restTemplate.getForObject("http://booking-service:8082/doctor/book/"+doctorId,String.class);
+    }
+
+    @RequestMapping("/create")
+    public List<MyUser> createUser(){
+        MyUser myUser = new MyUser(100L,"Pankaj");
+        userRepository.save(myUser);
+        return getAllUsers();
+    }
+
+    @RequestMapping("/test")
+    public List<MyUser> testApp(){
+        List<MyUser> testUserList = new ArrayList<>();
+        testUserList.add(new MyUser(100L,"John"));
+        testUserList.add(new MyUser(200L,"Peter"));
+        return testUserList;
     }
 }
